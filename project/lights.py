a6b8cf20e1745d963d001f8f82f74e2c3d420f92
Demo User <debian@beaglebone.localdomain>
2022-12-05 03:23:02 +0000

    code update

--- project/lights.py
+++ project/lights.py
@@ -20,33 +20,42 @@ veml7700 = adafruit_veml7700.VEML7700(i2c)
 PIR_pin = "P1_02"
 
 # Time delay between SPI writes
-timeDelay     = 0.00005
+timeDelay     = 0.00006
 
 
 # ------------------------------------------------------------------------
 # Demo Functions
 # ------------------------------------------------------------------------
 
-def ledStringRandom(spi_bus):
-    """Generate random colors on LED string"""
-    spi_bus.writebytes([0xFF, 0, 0])
-    time.sleep(timeDelay)
-    
-    spi_bus.writebytes([0, 0xFF, 0])
-    time.sleep(timeDelay)
-# End def
+def ledStringOn(spi_bus):
+    """Generate colors on LED string based on brightness of room"""
+    if veml7700.lux < 100
+        spi_bus.writebytes([0x8B, 0x0, 0x0]) #red
+        time.sleep(timeDelay)
+    else if veml7700.lux < 250
+        spi_bus.writebytes([0x0, 0x0, 0xFF]) #blue
+        time.sleep(timeDelay)
+    else if veml7700.lux < 500
+        spi_bus.writebytes([0x22, 0x8B, 0x22]) #green
+        time.sleep(timeDelay)
+    else if veml7700.lux < 750
+        spi_bus.writebytes([0xFF, 0x8C, 0x0]) #orange
+        time.sleep(timeDelay)
+    else 
+        spi_bus.writebytes([0xF8, 0xF8, 0xFF]) #white
+        time.sleep(timeDelay)
+
 
 
 def ledStringOff(spi_bus):
     """Turn off the LED String"""
     spi_bus.writebytes([0, 0, 0])
     time.sleep(timeDelay)
-# End def
 
  
 # ------------------------------------------------------------------------
 # Main script
-# -------------------------------------------------------------- ----------
+# ------------------------------------------------------------------------
 
 if __name__ == '__main__':
  
@@ -59,11 +68,13 @@ if __name__ == '__main__':
     # Main loop to turn on/off the LED string
     while(True):
        if GPIO.input(PIR_pin):
-           ledStringRandom(spi)
+           ledStringOn(spi)
        else:
            ledStringOff(spi)
 
     # Clean up the SPI and GPIO
     spi.close()
     GPIO.cleanup()
+
+
 
